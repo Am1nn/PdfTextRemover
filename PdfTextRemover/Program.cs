034a7e0e11468ea88f6a4866857d32556b41863e
Amin Bennayev <129307929+Am1nn@users.noreply.github.com>
2024-10-11 19:24:08 +0400

    Add project files.

---
+++ PdfTextRemover/Program.cs
@@ -0,0 +1,36 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string inputPdf = @"C:\Users\Amin\Desktop\HTML-to-PDF.pdf";
+        string outputPdf = @"pdf.pdf";
+
+        using (PdfReader reader = new PdfReader(inputPdf))
+        using (PdfWriter writer = new PdfWriter(outputPdf))
+        using (PdfDocument pdfDoc = new PdfDocument(reader, writer))
+        {
+
+            for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
+            {
+                var page = pdfDoc.GetPage(i);
+
+                PdfCanvas canvas = new PdfCanvas(page);
+
+                
+                float x = 0; 
+                float y = 780; 
+                float width = 500; 
+                float height = 20;
+
+                canvas.SetFillColor(iText.Kernel.Colors.ColorConstants.WHITE);
+                canvas.Rectangle(x, y, width, height);
+                canvas.Fill();
+            }
+        }
+    }
+}
